a254e515f988ef0c9c580b13857d7b46140fd675
hgao080 <hgao080@aucklanduni.ac.nz>
2024-07-04 21:47:54 +1200

    Added UI to rps game

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-console.log(playGame());
+const btns = document.querySelectorAll("button");
+const move = document.getElementById("move");
+const humanScoreDOM = document.getElementById("playerScore")
+const computerScoreDOM = document.getElementById("computerScore")
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    playRound(btn.textContent.toLowerCase(), getComputerChoice());
+    computerScoreDOM.textContent = computerScore;
+    humanScoreDOM.textContent = humanScore;
+  })
+})
+
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
@@ -13,83 +27,62 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let playerChoice = prompt("Would you like to play ROCK, PAPER or SCISSORS?");
-
-  switch (playerChoice.toLowerCase()) {
-    case "rock":
-      return "rock";
-    case "paper":
-      return "paper";
-    case "scissors":
-      return "scissors";
-    default:
-      return "invalid input";
+function showResults() {
+  if (humanScore > computerScore) {
+    move.textContent = "You Win!";
+    return "";
+  }
+  if (humanScore < computerScore) {
+    move.textContent = "You Lose!";
+    return "";
   }
 }
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-
-  if (humanScore > computerScore) {
-    console.log("You Win!")
-    return;
-  }
-  if (humanScore < computerScore) {
-    console.log("You Lose!")
-    return;
-  }
-  console.log("Draw!")
-
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice == "rock") {
-      if (computerChoice == "rock") {
-        console.log("Draw! Both played Rock")
-      }
-      if (computerChoice == "paper") {
-        console.log("You Lose! Paper beats Rock")
-        computerScore++;
-      }
-      if (computerChoice == "scissors") {
-        console.log("You Win! Rock beats Scissors")
-        humanScore++;
-      }
-      return;
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "rock") {
+    if (computerChoice == "rock") {
+      move.textContent = "Draw! Both played Rock";
     }
-  
-    if (humanChoice == "paper") {
-      if (computerChoice == "rock") {
-        console.log("You Win! Paper beats Rock!")
-        humanScore++;
-      }
-      if (computerChoice == "paper") {
-        console.log("Draw! Both played Paper")
-      }
-      if (computerChoice == "scissors") {
-        console.log("You Lose! Scissors beats Paper")
-        computerScore++;
-      }
-      return;
+    if (computerChoice == "paper") {
+      move.textContent = "You Lose! Paper beats Rock";
+      computerScore++;
     }
-  
-    if (humanChoice == "scissors") {
-      if (computerChoice == "rock") {
-        console.log("You Lose! Rock beats Scissors")
-        computerScore++;
-      }
-      if (computerChoice == "paper") {
-        console.log("You Win! Scissors beats Paper")
-        humanScore++;
-      }
-      if (computerChoice == "scissors") {
-        console.log("Draw! Both played Scissors")
-      }
-      return;
+    if (computerChoice == "scissors") {
+      move.textContent = "You Win! Rock beats Scissors";
+      humanScore++;
     }
   }
+
+  if (humanChoice == "paper") {
+    if (computerChoice == "rock") {
+      move.textContent = "You Win! Paper beats Rock!";
+      humanScore++;
+    }
+    if (computerChoice == "paper") {
+      move.textContent = "Draw! Both played Paper";
+    }
+    if (computerChoice == "scissors") {
+      move.textContent = "You Lose! Scissors beats Paper";
+      computerScore++;
+    }
+  }
+
+  if (humanChoice == "scissors") {
+    if (computerChoice == "rock") {
+      move.textContent = "You Lose! Rock beats Scissors";
+      computerScore++;
+    }
+    if (computerChoice == "paper") {
+      move.textContent = "You Win! Scissors beats Paper";
+      humanScore++;
+    }
+    if (computerChoice == "scissors") {
+      move.textContent = "Draw! Both played Scissors";
+    }
+  }
+
+  if (humanScore == 5 || computerScore == 5) {
+    showResults();
+  }
+  return;
 }
